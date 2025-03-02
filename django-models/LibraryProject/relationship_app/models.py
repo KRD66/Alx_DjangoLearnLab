1ea360098b62eb9a9c78c6d56e4e9b5088eee5f8
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-03-02 21:09:26 +0100

    Implementing Custom Permissions in Django

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -6,6 +6,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
+
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
@@ -16,6 +17,15 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
+
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can edit book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+    
     def __str__(self):
         return self.title
 
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import list_books , register 
+from .views import list_books , register , add_book, edit_book, delete_book
 from .views import  LibraryDetailView , admin_view, librarian_view, member_view
 
 urlpatterns = [
@@ -15,4 +15,7 @@ urlpatterns = [
     path('admin-view/', admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
+    path("books/add/", add_book, name="add_book"),
+    path("books/edit/<int:book_id>/", edit_book, name="edit_book"),
+    path("books/delete/<int:book_id>/", delete_book, name="delete_book"),
 ]
