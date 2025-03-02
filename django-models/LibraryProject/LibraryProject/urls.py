0f51d8af42b86ebe47965b01f3edae21988433dc
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-03-02 08:32:45 +0100

    Django Views and URL Configuration

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -23,7 +23,7 @@ from relationship_app.views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/', list_books, name='list_books'),  # URL for function-based view
+    path('books/', list_books, name='list_books'), 
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # URL for class-based view
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="logout.html"), name="logout"),
