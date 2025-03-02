c196da99f869ed3509793566dcf7fd9cb4cd978f
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-03-02 08:30:07 +0100

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,7 @@ from .models import Book
 
 from django.views.generic import DetailView
 from .models import Library
-
+from django.http import HttpResponse
 
 def list_books(request):
     books = Book.objects.all()  # Retrieve all books from the database
@@ -15,3 +15,15 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
     context_object_name = 'library'
+    
+    # relationship_app/views.py
+
+
+def list_books(request):
+    books = Book.objects.all()  # Retrieve all books from the database
+    response_text = "Books Available:\n\n"
+    
+    for book in books:
+        response_text += f"- {book.title} by {book.author.name}\n"
+    
+    return HttpResponse(response_text, content_type="text/plain")
