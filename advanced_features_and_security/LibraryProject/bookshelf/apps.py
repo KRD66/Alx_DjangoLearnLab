1e74aff3bf9436b775487c1f759152ce194289ba
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-03-04 19:40:37 +0100

    Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/bookshelf/apps.py
+++ advanced_features_and_security/LibraryProject/bookshelf/apps.py
@@ -1,6 +1,35 @@
 from django.apps import AppConfig
-
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from django.db.models.signals import post_migrate
 
 class BookshelfConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bookshelf'
+
+    def ready(self):
+        post_migrate.connect(create_groups_and_permissions, sender=self)
+
+def create_groups_and_permissions(sender, **kwargs):
+    from .models import Book  # Import your model
+
+    content_type = ContentType.objects.get_for_model(Book)
+
+    # Define required permissions
+    permissions = {
+        "can_view": Permission.objects.get(codename="can_view", content_type=content_type),
+        "can_create": Permission.objects.get(codename="can_create", content_type=content_type),
+        "can_edit": Permission.objects.get(codename="can_edit", content_type=content_type),
+        "can_delete": Permission.objects.get(codename="can_delete", content_type=content_type),
+    } 
+    
+    groups_permissions = {
+        "Admins": permissions.values(),  # Admins get all permissions
+        "Editors": [permissions["can_view"], permissions["can_create"], permissions["can_edit"]],
+        "Viewers": [permissions["can_view"]],
+    }
+
+    # Create groups and assign permissions
+    for group_name, perms in groups_permissions.items():
+        group, created = Group.objects.get_or_create(name=group_name)
+        group.permissions.set(perms)
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Book
+from .forms import BookForm 
 
 # Create your views here.
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect("book_list")  # Redirect to book list after editing
+    else:
+        form = BookForm(instance=book)
+
+    return render(request, "bookshelf/edit_book.html", {"form": form})
