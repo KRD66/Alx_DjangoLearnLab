e1624808e92b34eb7a9c05d7f41e68be7849c34c
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-03-18 23:39:00 +0100

    views.py modified

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics , permissions , filters , serializers
 from .models import Book
 from .serializers import BookSerializer
 from datetime import datetime
