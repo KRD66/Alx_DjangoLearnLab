c2e4675090c71edda4b9601ad0db39d140a88f6a
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-03-18 23:36:09 +0100

    permissions added

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,14 +2,23 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from datetime import datetime
 
 class BookListView(generics.ListAPIView):  # GET all books
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['author__name']
 
 class BookCreateView(generics.CreateAPIView):  # POST a new book
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    
+    def perform_create(self, serializer):
+        publication_year = serializer.validated_data['publication_year']
+        if publication_year > datetime.now().year:
+            raise serializers.ValidationError({'publication_year': "publication year can't be in the future."})
+        serializer.save()
 
 class BookDetailView(generics.RetrieveAPIView):  # GET a single book by ID
     queryset = Book.objects.all()
@@ -17,7 +26,11 @@ class BookDetailView(generics.RetrieveAPIView):  # GET a single book by ID
 class BookUpdateView(generics.UpdateAPIView):  # PUT/PATCH an existing book
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
 
 class BookDeleteView(generics.DestroyAPIView):  # DELETE a book
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
