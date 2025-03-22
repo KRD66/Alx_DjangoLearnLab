d5171e80bf38258e4a72986e37519f6b5ec46023
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-03-22 10:36:43 +0100

    added

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,9 +1,13 @@
-from django.contrib.auth.models import User
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone  # Import timezone for date handling
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)  # Ensure this points to User
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  
     created_at = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(default=timezone.now)  # Add this field
+
+    def __str__(self):
+        return self.title
