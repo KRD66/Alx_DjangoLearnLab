5c172b39632a53f06666f1243138fbdea66f56b8
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-03-23 21:38:40 +0100

    taggit added

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,11 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Post, Comment
+       
+from taggit.forms import TagField
+from taggit_labels.widgets import LabelWidget
+from .models import Post
+
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -13,13 +18,14 @@ class UserRegisterForm(UserCreationForm):
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
-
+    
     class Meta:
         model = User
         fields = ['username', 'email']
         
 class PostForm(forms.ModelForm):
-    
+    tags = TagField(widget=LabelWidget())
+
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
