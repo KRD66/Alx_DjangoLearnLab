db612fe8a8ee72c41c38f0deb2db1f1dc8b8ecca
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-03-28 13:46:48 +0100

    token added

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 
-User = get_user_model()  # Get the custom user model
+User = get_user_model().objects.create_user
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -11,11 +11,23 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
 
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)  # Ensure user creation
-        Token.objects.create(user=user)  # Generate auth token for new user
+def create(self, validated_data):
+        # Create the user properly using create_user()
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
+        
+        # Add additional fields if they exist
+        if 'bio' in validated_data:
+            user.bio = validated_data['bio']
+        if 'profile_picture' in validated_data:
+            user.profile_picture = validated_data['profile_picture']
+        
+        user.save()
+        Token.objects.create(user=user)  # Create auth token
         return user
-    
     
     
 class LoginSerializer(serializers.Serializer):
