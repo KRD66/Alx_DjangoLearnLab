b5bd0b58b19a99fccbf75abe32372f6acd9c718b
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-03-28 13:32:03 +0100

    urpls.py modified

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,8 +1,10 @@
+from django.contrib import admin
 from django.urls import path,include
 from .views import RegisterView, LoginView
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('api/', include("posts.urls"))
+    path('api/', include('posts.urls'))
 ]
