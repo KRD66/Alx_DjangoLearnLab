50f2d9cf65aac6b7b65522e1725c2743ecb9d9e7
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-03-29 14:26:14 +0100

    accounts app modified

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView,UserViewSet
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)  # Handles user-related operations
+
+
+urlpatterns =  [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('users/<int:pk>/follow/', UserViewSet.as_view({'post': 'follow'}), name='follow-user'),
+    path('users/<int:pk>/unfollow/', UserViewSet.as_view({'post': 'unfollow'}), name='unfollow-user'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('api/', include('posts.urls'))
+    
+    
 ]
