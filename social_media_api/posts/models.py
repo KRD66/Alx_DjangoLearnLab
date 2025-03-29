997d09db616205c5ef3ee63329c0fb7f8b962950
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-03-29 14:49:24 +0100

    like and notifications added

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,5 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User 
+from django.contrib.auth import get_user_model
+
+
+
+User = get_user_model()
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
@@ -7,6 +12,20 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+    def like_count(self):
+        """Returns the number of likes for this post"""
+        return self.likes.count()
+
+class Like(models.Model):
+    """Like model to track post likes"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name="likes", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')  # Prevent duplicate likes from the same user
 
     def __str__(self):
         return self.title
@@ -20,3 +39,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author.username} on {self.post.title}"
+
