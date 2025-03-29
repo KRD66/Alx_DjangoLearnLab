9016a753075c036b761562802e029dfb2708f2fe
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-03-29 15:49:04 +0100

    debug added

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  False
+"DEBUG =  False"
 
   # Load environment variables from a .env file
 
